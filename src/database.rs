336220c94db9b6d9cc44acdace45f8a07e3e443a
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-06-01 12:15:33 +0900

    add trait fn

--- src/database.rs
+++ src/database.rs
@@ -4,5 +4,6 @@ use sqlx::MySqlPool;
 #[async_trait]
 pub trait Database {
 	async fn init(&mut self, db: &MySqlPool) -> anyhow::Result<()>;
+	async fn database_name(&self, db: &MySqlPool) -> anyhow::Result<String>;
 	async fn table_names(&self, db: &MySqlPool) -> anyhow::Result<Vec<String>>;
 }
