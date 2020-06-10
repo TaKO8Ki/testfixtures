0a48993d3aaf81119d478f2dad0f019eb5d88a35
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-06-10 10:44:33 +0900

    fix linter warning

--- src/loader.rs
+++ src/loader.rs
@@ -134,7 +134,7 @@ where
                 for record in records {
                     let (sql, values) = self.build_insert_sql(&self.fixtures_files[index], record);
                     self.fixtures_files[index].insert_sqls.push(InsertSQL {
-                        sql: sql,
+                        sql,
                         params: values,
                     });
                 }
@@ -146,31 +146,29 @@ where
         let mut sql_columns = vec![];
         let mut sql_values = vec![];
         let mut values = vec![];
-        match &record {
-            Yaml::Hash(hash) => {
-                for (key, value) in hash {
-                    let value = match value {
-                        Yaml::String(v) => format!(r#""{}""#, v.to_string()),
-                        Yaml::Integer(v) => v.to_string(),
-                        _ => "".to_string(),
-                    };
-                    let key = match key {
-                        Yaml::String(k) => k.to_string(),
-                        Yaml::Integer(k) => k.to_string(),
-                        _ => "".to_string(),
-                    };
-                    sql_columns.push(key);
-                    if value.starts_with("RAW=") {
-                        sql_values.push(value.replace("RAW=", ""));
-                        continue;
-                    }
-
-                    sql_values.push("?".to_string());
-                    values.push(value);
+        if let Yaml::Hash(hash) = &record {
+            for (key, value) in hash {
+                let value = match value {
+                    Yaml::String(v) => format!(r#""{}""#, v.to_string()),
+                    Yaml::Integer(v) => v.to_string(),
+                    _ => "".to_string(),
+                };
+                let key = match key {
+                    Yaml::String(k) => k.to_string(),
+                    Yaml::Integer(k) => k.to_string(),
+                    _ => "".to_string(),
+                };
+                sql_columns.push(key);
+                if value.starts_with("RAW=") {
+                    sql_values.push(value.replace("RAW=", ""));
+                    continue;
                 }
+
+                sql_values.push("?".to_string());
+                values.push(value);
             }
-            _ => (),
-        }
+        };
+
         let sql_str = format!(
             "INSERT INTO {} ({}) VALUES ({})",
             file.file_stem(),
