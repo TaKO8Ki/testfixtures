3b85898c7073e75548cfd0cf242f5b9a23a02beb
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-06-17 10:38:01 +0900

    fix mysql query bind

--- src/mysql/helper.rs
+++ src/mysql/helper.rs
@@ -1,6 +1,10 @@
+use crate::fixture_file::FixtureFile;
 use crate::helper::Database as DB;
 use async_trait::async_trait;
-use sqlx::{Connect, Connection, Database, Pool, Query};
+use sqlx::mysql::MySqlQueryAs;
+use sqlx::{
+    arguments::Arguments, mysql::MySqlArguments, MySql as M, MySqlConnection, MySqlPool, Query,
+};
 
 #[derive(Debug)]
 pub struct MySql {
@@ -14,20 +18,16 @@ impl Default for MySql {
 }
 
 #[async_trait]
-impl<T, C> DB<T, C> for MySql
-where
-    T: Database + Sync + Send,
-    C: Connection<Database = T> + Connect<Database = T> + Sync + Send,
-{
-    async fn init(&mut self, _pool: &Pool<C>) -> anyhow::Result<()> {
+impl DB<M, MySqlConnection> for MySql {
+    async fn init(&mut self, _pool: &MySqlPool) -> anyhow::Result<()> {
         Ok(())
     }
 
     // TODO: complete this function
-    // async fn database_name(&self, pool: &Pool<C>) -> anyhow::Result<String> {
-    //     let rec: (String,) = sqlx::query!("SELECT DATABASE()").fetch_one(db).await?;
-    //     Ok(rec.0)
-    // }
+    async fn database_name(&self, pool: &MySqlPool) -> anyhow::Result<String> {
+        let rec: (String,) = sqlx::query_as("SELECT DATABASE()").fetch_one(pool).await?;
+        Ok(rec.0)
+    }
 
     // TODO: complete this function
     // async fn table_names(&self, pool: &Pool<C>) -> anyhow::Result<Vec<String>> {
@@ -51,14 +51,31 @@ where
 
     async fn with_transaction<'b>(
         &self,
-        pool: &Pool<C>,
-        queries: Vec<Query<'b, T>>,
+        pool: &MySqlPool,
+        fixture_files: &Vec<FixtureFile>,
     ) -> anyhow::Result<()> {
         let mut tx = pool.begin().await?;
         let result: anyhow::Result<()> = async {
             sqlx::query("SET FOREIGN_KEY_CHECKS = 0")
                 .execute(&mut tx)
                 .await?;
+            let mut queries = vec![];
+            let delete_queries: Vec<String> = fixture_files.iter().map(|x| (x.delete())).collect();
+            let mut delete_queries: Vec<Query<'_, M>> =
+                delete_queries.iter().map(|x| sqlx::query(x)).collect();
+            queries.append(&mut delete_queries);
+
+            for fixtures_file in fixture_files {
+                for i in &fixtures_file.insert_sqls {
+                    let mut args = MySqlArguments::default();
+                    for i in &i.params {
+                        args.add(i)
+                    }
+                    let query = sqlx::query(i.sql.as_str()).bind_all(args);
+                    queries.push(query)
+                }
+            }
+
             for query in queries {
                 query.execute(&mut tx).await?;
             }
