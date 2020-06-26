c292177b1d973debaa603d8a16e54907e3e46b89
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-06-27 00:10:20 +0900

    remove comment

--- src/postgresql/helper.rs
+++ src/postgresql/helper.rs
@@ -10,18 +10,7 @@ use sqlx::{
 /// **PostgreSQL** helper.
 pub struct PostgreSql {
     pub tables: Vec<String>,
-    // pub use_alter_constraint: bool,
-    // pub skip_reset_sequences: bool,
-    // pub reset_sequences_to:   u32,
-    // pub sequences:                Vec<String>,
-    // pub nonDeferrableConstraints: Vec<PgConstraint>,
-    // pub tablesChecksum:           map[string]string
 }
-
-// pub struct PgConstraint {
-//     table_name: String,
-//     constraint_name: String,
-// }
 
 impl Default for PostgreSql {
     fn default() -> Self {
